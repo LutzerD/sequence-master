6d8592d3553044c3da2c8664892741d10034b236
LutzerD <nautiouscat@gmail.com>
2021-07-24 15:42:35 -0600

    Settings button colors, spacing

--- memorize/components/settings.js
+++ memorize/components/settings.js
@@ -12,55 +12,60 @@ import {
 import Environment from "../../environment";
 
 const buttonOnColor = undefined;
-const buttonOffColor = 'white';
+const buttonOffColor = "#841584";
 
+const StyledButton = (props) => {
+  return (
+    <View style={props.style}>
+      <Button title={props.title} color={props.color} onPress={props.onPress}></Button>
+    </View>
+  );
+};
 const SettingsComponent = (props) => {
   return (
     <View style={{ flex: 1 }}>
-      <View style={[styles.centerChildren, SettingsStyles.button]}>
+      <View style={styles.centerChildren}>
         <View style={{ flex: 10 }} />
         {Environment.prod ? (
-          <Button
+          <StyledButton
             title="Reset Everything"
             onPress={() => props.dispatch({ type: RESET_ALL })}
             style={SettingsStyles.button}
-          ></Button>
+          ></StyledButton>
         ) : null}
-        <Button
+        <StyledButton
           title="Reset Highscores"
           onPress={() => props.dispatch({ type: RESET_HIGHSCORES })}
           style={SettingsStyles.button}
-        ></Button>
-        <View style={styles.rowDirection}>
-          <Button
-            title={"REVIEW TTS " + (props.settings.tts.review ? "ON" : "OFF")}
-            onPress={() => props.dispatch({ type: REVIEW_TTS_TOGGLE })}
-            style={SettingsStyles.button}
-            color={
-              props.settings.tts.review
-                ? SettingsStyles.buttonOn
-                : SettingsStyles.buttonOFF
-            }
-          ></Button>
-          <Button
-            title={
-              "INTRODUCTION TTS " + (props.settings.tts.intro ? "ON" : "OFF")
-            }
-            onPress={() => props.dispatch({ type: INTRO_TTS_TOGGLE })}
-            style={SettingsStyles.button}
-            color={
-              props.settings.tts.intro
-                ? SettingsStyles.buttonOn
-                : SettingsStyles.buttonOFF
-            }
-          ></Button>
-        </View>
+        ></StyledButton>
+        <StyledButton
+          title={"REVIEW TTS " + (props.settings.tts.review ? "ON" : "OFF")}
+          onPress={() => props.dispatch({ type: REVIEW_TTS_TOGGLE })}
+          style={SettingsStyles.button}
+          color={
+            props.settings.tts.review
+              ? buttonOnColor
+              : buttonOffColor
+          }
+        ></StyledButton>
+        <StyledButton
+          title={
+            "INTRODUCTION TTS " + (props.settings.tts.intro ? "ON" : "OFF")
+          }
+          onPress={() => props.dispatch({ type: INTRO_TTS_TOGGLE })}
+          style={SettingsStyles.button}
+          color={
+            props.settings.tts.intro
+              ? buttonOnColor
+              : buttonOffColor
+          }
+        ></StyledButton>
         <View style={{ flex: 10 }} />
-        <Button
+        <StyledButton
           title="Return"
           onPress={() => props.dispatch({ type: GOTO_PREVIOUS })}
           style={SettingsStyles.button}
-        ></Button>
+        ></StyledButton>
         <View style={{ flex: 1 }} />
       </View>
     </View>
@@ -72,5 +77,6 @@ export default SettingsComponent;
 const SettingsStyles = StyleSheet.create({
   button: {
     margin: 10,
+    padding: 10,
   },
 });
